729e42a1e704bdfe439eba12ed480484a3192b25
Brad Busch <65198733+AMdew73@users.noreply.github.com>
2020-05-12 18:53:54 -0500

    Add files via upload

--- LogicApp-SP2020.ps1
+++ LogicApp-SP2020.ps1
@@ -8,13 +8,12 @@ $resourceGroup = Get-AzResourceGroup | Out-GridView -PassThru -Title "Select the
 $resourceGroupName = $resourceGroup.ResourceGroupName
 $location = $resourceGroup.Location
 
-#Manually enter Host Pool Name#
-$hostPoolName = WIN10-STD
 
+$hostPoolName = Read-Host -Prompt "Enter Host Pool Name"
 $recurrenceInterval = Read-Host -Prompt "Enter how often you'd like the job to run in minutes, e.g. '15'"
-$beginPeakTime = Read-Host -Prompt "Enter the start time for peak hours in local time, e.g. 9:00"
+$beginPeakTime = Read-Host -Prompt "Enter the start time for peak hours in local time, e.g. 7:00"
 $endPeakTime = Read-Host -Prompt "Enter the end time for peak hours in local time, e.g. 18:00"
-$timeDifference = Read-Host -Prompt "Enter the time difference between local time and UTC in hours, e.g. +5:30"
+$timeDifference = Read-Host -Prompt "Enter the time difference between local time and UTC in hours, e.g. -5:00"
 $sessionThresholdPerCPU = Read-Host -Prompt "Enter the maximum number of sessions per CPU that will be used as a threshold to determine when new session host VMs need to be started during peak hours"
 $minimumNumberOfRdsh = Read-Host -Prompt "Enter the minimum number of session host VMs to keep running during off-peak hours"
 $limitSecondsToForceLogOffUser = Read-Host -Prompt "Enter the number of seconds to wait before automatically signing out users. If set to 0, users will be signed out immediately"
@@ -29,7 +28,7 @@ $connectionAssetName = $automationAccountConnection.Name
 $webHookURI = Read-Host -Prompt "Enter the URI of the WebHook returned by when you created the Azure Automation Account"
 $maintenanceTagName = Read-Host -Prompt "Enter the name of the Tag associated with VMs you don't want to be managed by this scaling tool"
 
-.\createazurelogicapp.ps1 -ResourceGroupName $resourceGroupName `
+.\createazurelogicapp-SP2020.ps1 -ResourceGroupName $resourceGroupName `
   -AADTenantID $aadTenantId `
   -SubscriptionID $subscriptionId `
   -HostPoolName $hostPoolName `
